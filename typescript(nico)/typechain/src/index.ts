29f03eff448a0a1634e789a1f89f208bbee43cfa
leesujin <jean0518@naver.com>
2022-10-06 01:39:49 +0900

    2022.10.06 tyscript blockchain

--- typescript(nico)/typechain/src/index.ts
+++ typescript(nico)/typechain/src/index.ts
@@ -1,6 +1,53 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string): string {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlock() {
+    return [...this.blocks];
+  }
+}
+
+const blcokchain = new Blockchain();
+
+blcokchain.addBlock("First one");
+blcokchain.addBlock("second one");
+blcokchain.addBlock("third one");
+
+console.log(blcokchain.getBlock());
